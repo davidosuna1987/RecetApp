9215a822bd4ba383bcc1a3863e9cd08443aeb639
David Osuna <davidosuna1987@gmail.com>
2018-05-26 20:06:41 +0200

    Removed custom Friendships

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,14 @@
 
 
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function(){
-    Route::get('/users/friendships/send/{id}', 'UserController@sendFriendshipRequest');
-    Route::get('/users/friendships/accept/{id}', 'UserController@acceptFriendshipRequest');
-    Route::get('/users/friendships/deny/{id}', 'UserController@denyFriendshipRequest');
 
-    Route::get('/test', function(){
-      $user = App\User::findOrFail(1);
-      dump('test', $user->test($user));
-      dump('isFriendWith(1)', $user->isFriendWith(1));
-      dump('hasFriendshipRequestFrom(1)', $user->hasFriendshipRequestFrom(1));
-      dump('hasSentFriendshipRequestTo(2)', $user->hasSentFriendshipRequestTo(2));
-      dump('getFriendship(2)', $user->getFriendship(2));
-      dump('getAllFriendships()', $user->getAllFriendships());
-      dump('getPendingFriendships()', $user->getPendingFriendships());
-      dump('getAcceptedFriendships()', $user->getAcceptedFriendships());
-      dump('getFriends()', $user->getFriends());
-      dump('getFriendsCount()', $user->getFriendsCount());
-      dump('getPendingFriendshipsCount()', $user->getPendingFriendshipsCount());
-    });
 });
